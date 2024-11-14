817f5f1458a6485cec2ec0fb9e62283c2c82fb51
nir-tzur <61886612+nir-tzur@users.noreply.github.com>
2024-11-14 21:04:18 +0200

    submission for hw 1

--- Bill3.java
+++ Bill3.java
@@ -1,13 +1,12 @@
 // Splits a restaurant bill evenly among three diners.
 public class Bill3 {
 	public static void main(String[] args) { 
-		String name1 = args[0];
-        String name2 = args[1];
-        String name3 = args[2];
+        String diner1 = args[2];
+        String diner2 = args[1];
+        String diner3 = args[0];
         int totalAmount = Integer.parseInt(args[3]);
 
         double amountPerDiner = Math.ceil(totalAmount / 3.0);
-        System.out.println("Dear " + name1 + ", " + name2 + ", and " + name3 + ": pay " + amountPerDiner + " Shekels each.");
-
+        System.out.println("Dear " + diner1 + ", " + diner2 + ", and " + diner3 + ": pay " + amountPerDiner + " Shekels each.");
 	}
 }
--- TimeFormat.java
+++ TimeFormat.java
@@ -1,12 +1,14 @@
 // Represents the hh:mm time format using an AM/PM format. 
 public class TimeFormat {
 	public static void main(String[] args) {
-        int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
-        int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
-        String period = (hours < 12) ? "AM" : "PM";
-        hours = (hours == 0) ? 12 : (hours > 12) ? hours - 12 : hours;
-        System.out.print(hours + ":");
-        System.out.print((minutes < 10 ? "0" : "") + minutes + " ");
-        System.out.println(period);
+                int hours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
+                int minutes = Integer.parseInt("" + args[0].charAt(3) + args[0].charAt(4));
+        
+                String period = (hours < 12) ? "AM" : "PM";
+                hours = (hours == 0) ? 12 : (hours > 12) ? hours - 12 : hours;
+        
+                System.out.print(hours + ":");
+                System.out.print((minutes < 10 ? "0" : "") + minutes + " ");
+                System.out.println(period);
 	}
 }
