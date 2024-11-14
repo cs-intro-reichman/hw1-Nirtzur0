72e743376a905fee45badf2b1bfb1eb07998eeba
nir-tzur <61886612+nir-tzur@users.noreply.github.com>
2024-11-14 20:55:29 +0200

    submission for hw 1

--- Ascend.java
+++ Ascend.java
@@ -2,6 +2,18 @@
 // and prints them in increasing order.
 public class Ascend {
 	public static void main(String[] args) {
-		// Replace this comment with your code
-	}
+        int limit = Integer.parseInt(args[0]);
+
+        int a = (int) (Math.random() * limit);
+        int b = (int) (Math.random() * limit);
+        int c = (int) (Math.random() * limit);
+
+        System.out.println(a + " " + b + " " + c);
+
+        int min = Math.min(a, Math.min(b, c));
+        int max = Math.max(a, Math.max(b, c));
+        int mid = a + b + c - min - max;
+
+        System.out.println(min + " " + mid + " " + max);	
+        }
 }
--- Demo0.java
+++ Demo0.java
@@ -1,9 +1,14 @@
 // Prints the numbers 0 to 5.
 // (Same program as shown in lecture 1-1).
-public class PrintTo5 {
+public class Demo0 {
 	public static void main(String[] args) {
-		// Declares an int variable, and sets it to 1
+        // Declares an int variable and sets it to 0
         int i = 0;
-        // Complete the program's code here:
+        // Loop to print numbers from 0 to 5
+        while (i <= 5) {
+            System.out.println(i);
+            i++; // Increment i by 1
+        }
+        System.out.println("Done");
     }
  }
